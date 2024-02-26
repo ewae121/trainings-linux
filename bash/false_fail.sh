74e4b07f5cc672f59c34361af76544e75313475f
Laurent Bousquet <laurent.bousquet@continental-corporation.com>
2024-02-26 16:56:33 +0100

    review before pres

--- bash/false_fail.sh
+++ bash/false_fail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -euo pipefail
 
 echo toto
 false | tee -a
--- bash/trainer.sh
+++ bash/trainer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -euo pipefail
+
 REAL_PATH=$(realpath "$0")
 W=$(dirname "${REAL_PATH}")
 
-POKEMONS_FOLDER="${W}"/pokemons
-PROPERTIES_FILE_SUFFIX="_properties.json"
+POKEMONS_FOLDER="${W}"/new_pokemons
+
+POKEMON_LIST_FILE="${POKEMONS_FOLDER}"/pokemons.json
+POKEMON_PROPERTIES_FILE_SUFFIX="_properties.json"
 
 send_activity_message (){
   message=$1
@@ -70,7 +73,7 @@ get_pokemon (){
   POKEMON_FOLDER="${POKEMONS_FOLDER}"/"$pokemon_name"
   send_activity_message "Creating $pokemon_name in $POKEMON_FOLDER"
   mkdir -p "${POKEMON_FOLDER}"
-  properties_file="${POKEMON_FOLDER}"/"${pokemon_name}${PROPERTIES_FILE_SUFFIX}"
+  properties_file="${POKEMON_FOLDER}"/"${pokemon_name}${POKEMON_PROPERTIES_FILE_SUFFIX}"
 
   get_pokemon_properties "$pokemon_name" "$url" "$properties_file"
   get_pokemon_pictures "$properties_file" "$POKEMON_FOLDER"
@@ -78,16 +81,16 @@ get_pokemon (){
 
 get_pokemon_list (){
   send_activity_message "Getting pokemon list"
-  if [[ -f "${POKEMONS_FOLDER}"/pokemons.json ]]; then
-    send_activity_message "The pokemon list is already available in ${POKEMONS_FOLDER}"/pokemons.json
+  if [[ -f $POKEMON_LIST_FILE ]]; then
+    send_activity_message "The pokemon list is already available in $POKEMON_LIST_FILE"
     return
   fi
-  curl -s https://pokeapi.co/api/v2/pokemon?limit=12000 | jq > "${POKEMONS_FOLDER}"/pokemons.json
-  send_activity_message "the pokemon list is available in ${POKEMONS_FOLDER}"/pokemons.json
+  curl -s https://pokeapi.co/api/v2/pokemon?limit=12000 | jq > $POKEMON_LIST_FILE
+  send_activity_message "the pokemon list is available in $POKEMON_LIST_FILE"
 }
 
 download_pokemon_list (){
-  cat "${POKEMONS_FOLDER}"/pokemons.json | jq -r '.results[] | join(" ")' | while read -r pokemon url; do
+  cat "${POKEMON_LIST_FILE}" | jq -r '.results[] | join(" ")' | while read -r pokemon url; do
     get_pokemon "$pokemon" "$url"
   done
 }
@@ -98,21 +101,12 @@ init_pokemons_folder
 while [[ $# -gt 0 ]]; do
   case $1 in
     -l|--list)
-      download_pokemon_list
-      exit 0
-      ;;
-    -a|--download-pokemon-list)
       get_pokemon_list
       download_pokemon_list
       exit 0
       ;;
-    -p|--download-pokemon)
-      POKEMON="$2"
-      shift # past argument
-      shift # past value
-      ;;
     -*|--*|*)
-      echo "Unknown option $1"
+      echo "Unknown option $1;"
       exit 1
       ;;
   esac
